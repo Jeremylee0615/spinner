f01eaebf9e45d66f393f30cd88696896525777a4
Jeremy Lee <jeremylee0615@gmail.com>
2023-02-18 16:40:30 -0800

    Animated ' / ' spinner

--- spinner1.js
+++ spinner1.js
@@ -1,34 +1,34 @@
-let delay = 250;
+let spinnerDelay = 250;
 
 // 2 cylces with no rest in between each cycles
 process.stdout.write('hello from spinner1.js... \rheyyy\n');
 setTimeout(() => {
   process.stdout.write('\r|   ');
-}, delay + 1000);
+}, spinnerDelay + 1000);
 setTimeout(() => {
   process.stdout.write('\r/   ');
-}, (delay * 2) + 1000);
+}, (spinnerDelay * 2) + 1000);
 setTimeout(() => {
   process.stdout.write('\r-   ');
-}, (delay * 3) + 1000);
+}, (spinnerDelay * 3) + 1000);
 setTimeout(() => {
   process.stdout.write('\r\\   ');
-}, (delay * 4) + 1000);
+}, (spinnerDelay * 4) + 1000);
 setTimeout(() => {
   process.stdout.write('\r|   ');
-}, (delay * 5));
+}, (spinnerDelay * 5));
 setTimeout(() => {
   process.stdout.write('\r|   ');
-}, (delay * 6) + 1000);
+}, (spinnerDelay * 6) + 1000);
 setTimeout(() => {
   process.stdout.write('\r/   ');
-}, (delay * 7) + 1000);
+}, (spinnerDelay * 7) + 1000);
 setTimeout(() => {
   process.stdout.write('\r-   ');
-}, (delay * 8) + 1000);
+}, (spinnerDelay * 8) + 1000);
 setTimeout(() => {
   process.stdout.write('\r\\   ');
-}, (delay * 9) + 1000);
+}, (spinnerDelay * 9) + 1000);
 setTimeout(() => {
   process.stdout.write('\r|   ');
-}, (delay * 10) + 1000);
+}, (spinnerDelay * 10) + 1000);
